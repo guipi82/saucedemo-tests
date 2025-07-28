6749cb6f33a5e521fc155f713e870f0233667970
guipi82 <34303757+guipi82@users.noreply.github.com>
2025-07-29 01:26:46 +0200

    Update PageNavigationSteps.java
    Added PageNavigationSteps()

--- src/test/java/stepdefinitions/PageNavigationSteps.java
+++ src/test/java/stepdefinitions/PageNavigationSteps.java
@@ -2,57 +2,48 @@ package stepdefinitions;
 
 import static org.junit.Assert.assertEquals;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import pages.CartPage;
+
+import org.openqa.selenium.WebDriver;
+
 import pages.ProductsPage;
 import utils.Hooks;
 
 public class PageNavigationSteps {
-    
-    WebDriver driver; // Get the WebDriver instance from Hooks
-    ProductsPage productsPage; // Create an instance of ProductsPage
-    
+
+    WebDriver driver;
+    ProductsPage productsPage;
+
+    public PageNavigationSteps() {
+        this.driver = Hooks.getDriver();           // zentrale Initialisierung
+        this.productsPage = new ProductsPage(driver);
+    }
 
     @When("der Benutzer wählt \"About\" aus")
     public void userNavigatesToProductsPage() {
-        driver = Hooks.getDriver();
-        productsPage = new ProductsPage(driver);
         productsPage.clickOnAbout();
     }
 
     @Then("wird die About-Seite geöffnet")
     public void userSeesProductsDisplayed() {
-        // Verify that the About page is displayed
-        boolean isAboutPageDisplayed = productsPage.assertAboutPageVisible();    
-        // Assert that the About page is displayed
+        boolean isAboutPageDisplayed = productsPage.assertAboutPageVisible();
         assertEquals("Die About-Seite wurde nicht angezeigt.", true, isAboutPageDisplayed);
-    } 
-
+    }
 
     @When("der Benutzer klickt auf \"Add to cart\" im Warenkorb")
     public void userAddsProductToCart() {
-        // Click on the "Add to Cart" button in the cart
         productsPage.addToCart();
     }
 
     @And("der Benutzer klickt auf das Burger-Menü im Warenkorb")
     public void userClicksOnBurgerMenuInCart() {
-        // Click on the burger menu icon in the cart
         productsPage.clickOnBurgerMenu();
-    }   
+    }
 
     @When("der Benutzer wählt \"Reset App State\" aus")
     public void userClicksOnResetAppState() {
-        // Click on the "Reset App State" link
         productsPage.clickOnResetAppState();
     }
 }
