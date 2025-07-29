15acb7be69642b50a193424b7511b40a8e982b23
guipi82 <34303757+guipi82@users.noreply.github.com>
2025-07-29 06:12:46 +0200

    Update TestRunner.java
    check

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,10 +4,13 @@ import io.cucumber.junit.CucumberOptions;   // Import Cucumber options for test 
 import org.junit.runner.RunWith; // Import JUnit runner     
 @RunWith(Cucumber.class) // Specify that this class is a Cucumber test runner
 @CucumberOptions(
-    features = "src/test/resources/features", // Path to the feature files
-    glue = {"stepdefinitions", "utils"}, // Package containing step definitions
-    plugin = {"pretty", "html:target/cucumber-reports.html"}, // Plugins for reporting
-    monochrome = true// Make console output more readable
+  plugin = {
+    "pretty",
+    "html:target/cucumber-html-reports",         // HTML-Report wird erzeugt
+    "json:target/cucumber-reports/cucumber.json" // Optional für JSON
+  },
+  features = "src/test/resources/features",
+  glue = {"stepdefinitions", "utils"}
 )
 public class TestRunner {
     
